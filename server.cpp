2a218b8bf5b3d7d403d6f4743bf19798771fb20a
Christopher Fujino <christopherfujino@gmail.com>
2026-02-13 23:31:40 -0800

    use system rather than fork + exec

--- server.cpp
+++ server.cpp
@@ -1,4 +1,4 @@
-#include <cstdio> // for printf()
+#include <cstdio>  // for printf()
 #include <cstring> // for strcmp()
 #include <ctime>
 #include <format>
@@ -11,35 +11,18 @@ import M;
 
 using namespace __CHRIS_MONOREPO__CPP__IPC__SHARED;
 
-void _exec(std::vector<std::string> cmd) {
+void _exec(std::string cmd) {
+  const char *cmd_s = cmd.c_str();
   std::time_t t = std::time(nullptr);
   auto time_obj = std::localtime(&t);
-  printf("[%02d:%02d:%02d]", time_obj->tm_hour, time_obj->tm_min,
-         time_obj->tm_sec);
-  for (auto &s : cmd) {
-    printf(" %s", s.c_str());
-  }
-  printf("\n");
-  int pid = fork();
+  printf("[%02d:%02d:%02d] %s\n", time_obj->tm_hour, time_obj->tm_min,
+         time_obj->tm_sec, cmd_s);
 
-  if (pid < 0) {
-    throw std::runtime_error("Failure to fork");
-  } else if (pid == 0) {
-    // It's fine we're allocating from heap, since we're about to exec
-    const char **v = new const char *[cmd.size() + 2];
-    size_t i = 0;
-    for (; i < cmd.size(); i++) {
-      v[i] = cmd[i].c_str();
-    }
-    v[i] = NULL;
-    execvp(cmd[0].c_str(), (char **)v);
-  } else {
-    int status = 0;
-    int options = 0;
-    waitpid(pid, &status, options);
-    // TODO check status
-    printf("\n");
+  int result = system(cmd_s);
+  if (result != 0) {
+    throw std::runtime_error(std::format("Command failed with {}\n", result));
   }
+  printf("\nSuccess!\n\n");
 }
 
 int main(int argc, char **argv) {
@@ -54,11 +37,21 @@ int main(int argc, char **argv) {
     }
   }
 
-  if (targetCommand.size() == 0) {
-    fprintf(stderr, "Usage: ipcr-server -- [COMMAND] [ARGS]...");
+  if (targetCommand.size() != 1) {
+    if (targetCommand.size() == 0) {
+      fprintf(stderr, "Usage: ipc-runner-server -- [COMMAND_STRING]\n");
+    } else {
+      fprintf(stderr, "Usage: ipc-runner-server -- [COMMAND_STRING]\n\n");
+      fprintf(
+          stderr,
+          "Your desired command and its arguments should be a single string.\n");
+      // TODO construct the string programmatically
+    }
+    return 1;
   }
 
   printf("Starting ipc-runner-server...\n\n");
+
   IPC::create();
   IPC::ReadChannel ipc = IPC::ReadChannel();
 
@@ -66,7 +59,7 @@ int main(int argc, char **argv) {
     auto cmd = ipc.read();
     switch (cmd) {
     case Command::BUILD:
-      _exec(targetCommand);
+      _exec(targetCommand[0]);
       break;
     default:
       throw std::runtime_error(
